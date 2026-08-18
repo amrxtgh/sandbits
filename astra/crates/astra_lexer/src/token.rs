f4b5dd30f37acc905a665a71d5b40ba6e26c8b1f
amrxtgh <ghimireamrit619@gmail.com>
2026-08-19 02:31:56 +0545

    is this enough something says that spanned wrapper should be add idk what that is

--- astra/crates/astra_lexer/src/token.rs
+++ astra/crates/astra_lexer/src/token.rs
@@ -1,9 +1,28 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
+    // Keywords
     Let,
+    Func,
+    Return,
+
+    // values/names
     Identifier(String),
     Integer(i64),
 
+    // Operators
+    Plus,
+    Minus,
+    Star,
+    Slash,
     Equal,
+
+    // Punctuation
+    LeftParen,
+    RightParen,
+    LeftBrac,
+    RightBrac,
     Semicolon,
+
+    // Special
+    EoF,
 }
