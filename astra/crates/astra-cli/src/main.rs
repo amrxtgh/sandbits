cdc06e9ad1f8e0d1239cc6c96e13801d18402abd
amrxtgh <ghimireamrit619@gmail.com>
2026-08-18 19:38:12 +0545

    Initialize project

---
+++ astra/crates/astra-cli/src/main.rs
@@ -0,0 +1,15 @@
+pub struct Buffer<T> {
+    data: Vec<T>,
+}
+impl<T> Buffer<T> {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+    pub fn push(&mut self, value: T) {
+        self.data.push(value);
+    }
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+}
+fn main() {}
