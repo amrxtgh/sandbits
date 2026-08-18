81da376599777ea33a8b5fa92ffba8cc78e7eff1
amrxtgh <ghimireamrit619@gmail.com>
2026-08-19 00:01:57 +0545

    added(cli): i think we should have add cli earlier

--- astra/crates/astra_cli/src/main.rs
+++ astra/crates/astra_cli/src/main.rs
@@ -1,9 +1,24 @@
 use astra_lexer::lexer::Lexer;
-
+use std::{env, fs};
 fn main() {
     println!("Astra Compiler");
-    let source = " = ; ";
-    let mut lexer = Lexer::new(source);
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: astra <files>");
+        return;
+    }
+    let filename = &args[1];
+
+    let source = match fs::read_to_string(filename) {
+        Ok(source) => source,
+        Err(error) => {
+            eprintln!("failed to read {filename}: {error}");
+            return;
+        }
+    };
+    println!("{source}");
+    let mut lexer = Lexer::new(&source);
     let tokens = lexer.lex();
     println!("{tokens:#?}");
 }
