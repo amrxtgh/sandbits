36d2c35be05d11db5b545fdd105360a19a7e35b7
amrxtgh <ghimireamrit619@gmail.com>
2026-08-20 00:36:42 +0545

    feat: seperate logic

---
+++ astra/crates/astra_cli/src/driver.rs
@@ -0,0 +1,35 @@
+use crate::input::read_source;
+use astra_lexer::lexer::Lexer;
+use std::env;
+
+pub fn run() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("usage: astra <files>");
+        return;
+    }
+    let filename = &args[1];
+
+    // input does the file handling
+    let source = match read_source(filename) {
+        Ok(source) => source,
+        Err(error) => {
+            eprintln!("failed to read {filename}: {error}");
+            return;
+        }
+    };
+    println!("{source}");
+
+    // returns char of tokens and pos
+    let mut lexer = Lexer::new(&source);
+    // do the tokenization
+    match lexer.tokenize() {
+        Ok(token) => {
+            println!("{:#?}", token);
+        }
+        Err(error) => {
+            eprintln!("lexer error: {error}");
+        }
+    }
+}
