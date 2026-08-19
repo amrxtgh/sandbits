52012cd2eea679d7b774bf2354cda037cee74bfe
amrxtgh <ghimireamrit619@gmail.com>
2026-08-20 00:37:09 +0545

    main is main

--- astra/crates/astra_cli/src/main.rs
+++ astra/crates/astra_cli/src/main.rs
@@ -1,32 +1,6 @@
-use astra_lexer::lexer::Lexer;
-
-use std::{env, fs};
-
+mod driver;
+mod input;
 fn main() {
     println!("Astra Compiler");
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("usage: astra <files>");
-        return;
-    }
-    let filename = &args[1];
-
-    let source = match fs::read_to_string(filename) {
-        Ok(source) => source,
-        Err(error) => {
-            eprintln!("failed to read {filename}: {error}");
-            return;
-        }
-    };
-    println!("{source}");
-    let mut lexer = Lexer::new(&source);
-    match lexer.tokenize() {
-        Ok(token) => {
-            println!("{:#?}", token);
-        }
-        Err(error) => {
-            eprintln!("lexer error: {error}");
-        }
-    }
+    driver::run();
 }
