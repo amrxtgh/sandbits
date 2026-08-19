010058a92820c105cb31237acabbefa8eaa72f73
amrxtgh <ghimireamrit619@gmail.com>
2026-08-20 01:44:44 +0545

    feat: error for the unterminated string

--- astra/crates/astra_lexer/src/error.rs
+++ astra/crates/astra_lexer/src/error.rs
@@ -4,18 +4,24 @@ use std::fmt;
 pub enum LexerError {
     UnexpectedChar { char: char, position: usize },
     InvalidInteger { string: String, position: usize },
+    UnterminatedString { position: usize },
 }
 
 impl fmt::Display for LexerError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            LexerError::UnexpectedChar { char, position } => write!(
-                f,
-                "unexpected character '{}' at position {}",
-                char, position,
-            ),
+            LexerError::UnexpectedChar { char, position } => {
+                write!(
+                    f,
+                    "unexpected character '{}' at position {}",
+                    char, position,
+                )
+            }
             LexerError::InvalidInteger { string, position } => {
                 write!(f, "invalid integer '{}' at position {}", string, position)
+            }
+            LexerError::UnterminatedString { position } => {
+                write!(f, "unterminated string starting at position {}", position)
             }
         }
     }
